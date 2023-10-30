4cd48658986aa9c89d989f717240e216880f686e
Sami-47-el <105201168+Sami-47-el@users.noreply.github.com>
2023-10-30 11:07:10 +0100

    add projets

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+// mettre en place un input 
+// récuperer la valeur de l'input quand on siaisi du texte
+// appeler L'api TV maze avec cette valeur 
+// récuperer les résultats dans notre application 
+
+
+  return (
+    <>
+    <h1></h1>
+  
+    </>
+  )
+}
+
+export default App
